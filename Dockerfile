0b62b7830ac18f50c2508c050dd7221ff913c48d
user1 <user1@SDEV>
2022-10-14 17:20:27 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:16
+WORKDIR /game
+
+COPY package.json ./
+RUN npm install
+
+COPY . .
+EXPOSE 8080
+CMD ["node" , "server.js"]
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res)
+{
+    res.sendFile('/index.html');
+});
+
+io.on('connection', function (socket)
+{
+    console.log('USER CONNECTED!');
+    socket.on('disconnect', function()
+    {
+        console.log('USER DISCONNECTED!');
+    });
+});
+
+
+server.listen(8080, function ()
+{
+  console.log(`Listening on ${server.address().port}`);
+});
