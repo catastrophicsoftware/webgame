f882eb0947fe1c87fd4ab25b8792f146dcc4fa7d
user1 <user1@SDEV>
2022-10-16 04:40:28 -0700

    implemented multiplayer skeleton, movable sprites

--- js/game.js
+++ js/game.js
@@ -20,15 +20,145 @@ var game = new Phaser.Game(config);
 
 function preload()
 {
-
+    this.load.image('player_sprite','assets/player.png');
+    this.load.image('your_sprite','assets/your_player.png')
 }
 
 function create()
 {
+    var self = this;
     this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+
+
+    this.socket.on('players',function(players)
+    {
+        Object.keys(players).forEach(function(id)
+        {
+            if(players[id].playerId === self.socket.id)
+            {
+                console.log("SPAWNED YOUR PLAYER!");
+                addPlayer(self,players[id]);
+            }
+            else
+            {
+                console.log("SYNCING OTHER PLAYER...");
+                addOtherPlayers(self,players[id]);
+            }
+        });
+    });
+
+    this.socket.on('new_player',function(playerInfo)
+    {
+        addOtherPlayers(self,playerInfo);
+    });
+
+    this.socket.on('disconnect',function(playerId)
+    {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer)
+        {
+            if(playerId == otherPlayer.playerId)
+            {
+                otherPlayer.destroy();
+            }
+        });
+    });
+
+
+    this.socket.on('playerMoved', function (playerInfo)
+    {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer)
+        {
+            if (playerInfo.playerId === otherPlayer.playerId)
+            {
+                otherPlayer.setRotation(playerInfo.rotation);
+                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+            }
+        });
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update()
 {
+    if (this.ship)
+    {
+        if (this.cursors.left.isDown)
+        {
+          this.ship.setAngularVelocity(-150);
+        }
+        else if (this.cursors.right.isDown)
+        {
+          this.ship.setAngularVelocity(150);
+        } 
+        else 
+        {
+          this.ship.setAngularVelocity(0);
+        }
+      
+        if (this.cursors.up.isDown) 
+        {
+          this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+        } 
+        else 
+        {
+          this.ship.setAcceleration(0);
+        }
+      
+        //this.physics.world.wrap(this.ship, 5);
+        //10-16-2022 0427 -- possible phaser version info, seemingly invalid function
 
+        var x = this.ship.x;
+        var y = this.ship.y;
+        var r = this.ship.rotation;
+        if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation))
+        {
+            this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+        }
+
+        // save old position data
+        this.ship.oldPosition = 
+        {
+            x: this.ship.x,
+            y: this.ship.y,
+            rotation: this.ship.rotation
+        };
+      }
 }
+
+
+function addPlayer(self,playerInfo)
+{
+    self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'your_sprite');
+    if (playerInfo.team === 'blue')
+    {
+      self.ship.setTint(0x0000ff);
+    }
+    else
+    {
+      self.ship.setTint(0xff0000);
+
+    }
+    self.ship.setDrag(100);
+    self.ship.setAngularDrag(100);
+    self.ship.setMaxVelocity(200);
+}
+
+
+function addOtherPlayers(self, playerInfo)
+{
+    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'player_sprite');
+
+    if (playerInfo.team === 'blue')
+    {
+      otherPlayer.setTint(0x0000ff);
+    } 
+    else
+    {
+      otherPlayer.setTint(0xff0000);
+    }
+
+    otherPlayer.playerId = playerInfo.playerId;
+    self.otherPlayers.add(otherPlayer);
+  }
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
+var players = {};
+
+
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res)
@@ -16,7 +19,33 @@ io.on('connection', function (socket)
     socket.on('disconnect', function()
     {
         console.log('USER DISCONNECTED!');
+        delete players[socket.id]
+        io.emit('disconnect',socket.id);
     });
+
+    // when a player moves, update the player data
+    socket.on('playerMovement', function (movementData)
+    {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        players[socket.id].rotation = movementData.rotation;
+        // emit a message to all players about the player that moved
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    var playerTeam = (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue';
+    console.log("PLAYER TEAM: " + playerTeam);
+
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: playerTeam
+    };
+
+    socket.emit('players',players);
+    socket.broadcast.emit('new_player',players[socket.id]);
 });
 
 
